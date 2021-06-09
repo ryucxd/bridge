2503213e5b1b6640427eee894866e37703cd8378
ryucxd <ryucxd@gmail.com>
2021-06-09 11:33:56 +0100

    added copy for engineer notes

--- bridge/Program.cs
+++ bridge/Program.cs
@@ -14,13 +14,18 @@ namespace bridge
         {
             string door_number = "67097"; //params
             string quote_number = "61586-1-1";//"60870"; //same 
-            string rev_number = "Packing List " + quote_number + "- Rev " + quote_number.Substring(quote_number.Length - 1);
+            string rev_number =  quote_number + "- Rev " + quote_number.Substring(quote_number.Length - 1);
             //door_number = args[0];
             //quote_number = args[1];
 
             string startFile = @"\\designsvr1\SOLIDWORKS\DWDevelopment\Specifications\" + quote_number + @"\documents\" + "DataOutput " + quote_number + "- Door Designer.DO"; ;//location
             string newFile = @"\\designsvr1\apps\Door Master\" + door_number + ".DO";
-            string packingFile = @"\\designsvr1\SOLIDWORKS\DWDevelopment\Specifications\" + quote_number + @"\documents\" + rev_number + ".xlsx"; //should be the default file path for the session for everyone
+            string packingFile = @"\\designsvr1\SOLIDWORKS\DWDevelopment\Specifications\" + quote_number + @"\documents\Packing List " + rev_number + ".xlsx"; //should be the default file path for the session for everyone
+            string engineerFile = @"\\designsvr1\SOLIDWORKS\DWDevelopment\Specifications\" + quote_number + @"\documents\Engineers Notes " + rev_number + ".xlsx";
+            string newPackingLocation = @"\\designsvr1\apps\bridge_jobcard\" + door_number + @"\Packing List " + door_number + ".xlsx";
+            string newEngineerLocation = @"\\designsvr1\apps\bridge_jobcard\" + door_number + @"\Engineer Notes " + door_number + ".xlsx";
+
+            System.IO.Directory.CreateDirectory(@"\\designsvr1\apps\bridge_jobcard\" + door_number); 
             //string fileName = "DataOutput " + quote_number + "- Door Designer.DO";
 
             //^^ we need to copy and move this file before editing 
@@ -49,11 +54,13 @@ namespace bridge
             Microsoft.Office.Interop.Excel.Worksheet xlWorksheet = xlWorkbook.Sheets[1]; // assume it is the first sheet
             Microsoft.Office.Interop.Excel.Range xlRange = xlWorksheet.UsedRange; // get the entire used range
 
-            xlWorksheet.Cells[5][7].Value2 = door_number.ToString() ;
-            xlWorksheet.SaveAs(packingFile);
+            xlWorksheet.Cells[5][7].Value2 = door_number.ToString();
+            xlWorksheet.SaveAs(newPackingLocation);
             xlWorkbook.Close(true); //close the excel sheet
             xlApp.Quit(); //close everything excel related so that theres no errors when the door program tries to connect 
-            
+
+            File.Copy(engineerFile, newEngineerLocation); // also move this one over with a new name
+
 
 
 
